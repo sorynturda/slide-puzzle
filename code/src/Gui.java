491460fef97839e79dca01be8c688ad758dc34db
Sorin Turda <s.turda12@gmail.com>
2023-11-09 18:26:26 +0200

    butoane adaugate

--- code/src/Gui.java
+++ code/src/Gui.java
@@ -5,22 +5,27 @@ import javax.swing.*;
 public class Gui extends JFrame {
     private JFrame window = new JFrame();
     private JButton but[] = new JButton[16];
-
     public Gui() {
         window.setSize(600, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel( new GridLayout(4, 4) );
+        JPanel panel = new JPanel( new GridLayout(4, 4));
         window.add(panel, BorderLayout.CENTER);
 
-        for (int i = 0; i < 15; i++) {
-            but[i] = new JButton(String.valueOf(i + 1));
-            but[i].setFont(new Font("Comic Sans", Font.BOLD, 60));
-            panel.add(but[i]);
-        }
-        but[15] = new JButton();
-        but[15].setFont(new Font("Comic Sans", Font.BOLD, 60));
-        panel.add(but[15]);
+        // for (int i = 0; i < 15; i++) {
+        //     but[i] = new JButton(String.valueOf(i + 1));
+        //     but[i].
+        //     panel.add(but[i]);
+        // }
+        // but[15] = new JButton();
+        // but[15].setFont(new Font("Comic Sans", Font.BOLD, 72));
+        // panel.add(but[15]);
+        panel.setLayout(null);
+        JButton but = new JButton("asdf");
+        but.setFont(new Font("Comic Sans", Font.BOLD, 72));
+        but.setBounds(0,0,150,150);
+        panel.add(but);
         window.setVisible(true);
+        but.setBounds(0,50,150,150);
 
     }
 
--- code/src/Numbers.java
+++ code/src/Numbers.java
@@ -1,11 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Numbers {
     private int number;
     private int x;
     private int y;
     private static final int n = 4;
     private static final int m = 4;
+    private static final int bounds[] = new int[] {0, 150, 300, 450};
+    private static final int size = 150;
     private boolean[] moves = new boolean[4];
-
+    private JButton button;
     /*
      *   allowed moves     up - 0
      *                  right - 1
@@ -16,6 +22,12 @@ public class Numbers {
         this.number = number;
         this.x = x;
         this.y = y;
+        if (number != 0)
+            this.button = new JButton(String.valueOf(number));
+        else
+            this.button = new JButton();
+        this.button.setFont(new Font("Comic Sans", Font.BOLD, 72));
+        this.button.setBounds(bounds[x], bounds[y], size, size);
     }
 
     public String getXY() {
@@ -28,6 +40,26 @@ public class Numbers {
 
     public int getY() {
         return this.y;
+    }
+
+    public JButton getButton() {
+        return this.button;
+    }
+
+    public void setButton(JButton button) {
+        this.button = button;
+    }
+
+    public void setButtonText(int number) {
+        this.button.setText(String.valueOf(number));
+    }
+
+    public void setButtonBounds(int x, int y) {
+        this.button.setBounds(bounds[x], bounds[y], size, size);
+    }
+
+    public void setButtonText() {
+        this.button.setText("");
     }
 
     public void setXY(int x, int y) {
